ce06ccca0b38592addbd717c4ddc87f2484525e0
Ощепков Антон <37110446+UrStanNightmare@users.noreply.github.com>
2022-06-05 21:17:35 +0700

    Initial commit.

---
+++ src/main/java/ru/urstannigtmare/mapsolver/Application.java
@@ -0,0 +1,16 @@
+package ru.urstannigtmare.mapsolver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application.class.getName());
+
+    public static void main(String[] args) {
+        log.info("Application start.");
+
+
+
+        log.info("Application end.");
+    }
+}
