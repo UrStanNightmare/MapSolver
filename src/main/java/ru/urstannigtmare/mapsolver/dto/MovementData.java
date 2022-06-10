32d041cb993c48601328159219efdbda27374e2a
Ощепков Антон <37110446+UrStanNightmare@users.noreply.github.com>
2022-06-10 21:50:51 +0700

    Добавил необходимые dto для работы с вершинами с приоритетом и стоимостями перемещения.

---
+++ src/main/java/ru/urstannigtmare/mapsolver/dto/MovementData.java
@@ -0,0 +1,39 @@
+package ru.urstannigtmare.mapsolver.dto;
+
+import java.util.List;
+import java.util.Map;
+
+public class MovementData {
+    private Map<String, Integer> movementData;
+    private List<String> availableTiles;
+
+    public MovementData(Map<String, Integer> movementData, List<String> availableTiles) {
+        this.movementData = movementData;
+        this.availableTiles = availableTiles;
+    }
+
+    public Map<String, Integer> getMovementData() {
+        return movementData;
+    }
+
+    public List<String> getAvailableTiles() {
+        return availableTiles;
+    }
+
+    public String getAvailableTilesString() {
+        String tiles = "";
+        for (String tile : this.availableTiles) {
+            tiles = tiles + tile + ",";
+        }
+
+        return tiles.substring(0, tiles.length() - 1);
+    }
+
+    public String getRegExpOfTiles() {
+        return "[" + this.getAvailableTilesString() + "]";
+    }
+
+    public Integer getMovementCostBySymlink(String symlink){
+        return this.movementData.get(symlink);
+    }
+}
